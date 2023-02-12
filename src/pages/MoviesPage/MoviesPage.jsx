f56676f63fb8e8194301561377d877b2e690b5ae
Marat Baikin <3371335@gmail.com>
2023-02-12 23:31:44 +0200

    upd

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,5 +1,56 @@
-const MoviesPage = () => {
-return (<h1>MoviesPage</h1>)
-}
+// import { ImSearch } from 'react-icons/im';
+//     import { toast } from 'react-toastify';
+    
+//     import { useState } from 'react';
+//     import PropTypes from 'prop-types';
+    
+//     import css from './Searchbar.module.css';
 
-export default MoviesPage
+// const MoviesPage = () => {
+        
+//     const Searchbar = ({ onSubmit }) => {
+//       const [searchQuery, setSearchQuery] = useState('');
+    
+//       const handleNameChange = event => {
+//         setSearchQuery(event.currentTarget.value.toLowerCase());
+//       };
+    
+//       const reset = () => {
+//         setSearchQuery('');
+//       };
+    
+//       const handleSubmit = event => {
+//         event.preventDefault();
+    
+//         if (searchQuery.trim() === '') {
+//           return toast.warn('Please enter a search term in the search box');
+//         }
+//         onSubmit(searchQuery);
+//         reset();
+//       };
+    
+//       return (
+//         <header className={css.Searchbar}>
+//           <form className={css.SearchForm} onSubmit={handleSubmit}>
+//             <button type="submit" className={css.SearchFormButton}>
+//               <ImSearch style={{ marginTop: 4 }} />
+//               <span className={css.SearchFormButtonLabel}>Search</span>
+//             </button>
+    
+//             <input
+//             //   className={css.SearchFormInput}
+//               type="text"
+//               name="searchQuery"
+//               value={searchQuery}
+//               onChange={handleNameChange}
+//               autoComplete="off"
+//               autoFocus
+//               placeholder="Search images and photos"
+//             />
+//           </form>
+//         </header>
+//       );
+//     };
+// }
+
+// export default MoviesPage
